7a50a2728a7c432f82cebfc14bf0961ebfb779f6
Tyler <tylerjklein24@gmail.com>
2026-04-26 10:20:33 -0400

    Changed warps for Petalburg Woods into Dynamic Warps that lead to it or Ilex Forest

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -176,7 +176,7 @@
 #define VAR_RUSTURF_TUNNEL_STATE                         0x409A
 #define VAR_SCORCHED_SLAB_STATE                          0x409B // Ttime100 added
 #define VAR_ELITE_4_STATE                                0x409C
-#define VAR_UNUSED_0x409D                                0x409D // Unused Var
+#define VAR_CELEBI_STATE                                 0x409D // Ttime100 added
 #define VAR_MOSSDEEP_SPACE_CENTER_STAIR_GUARD_STATE      0x409E
 #define VAR_MOSSDEEP_SPACE_CENTER_STATE                  0x409F
 #define VAR_SLATEPORT_HARBOR_STATE                       0x40A0
