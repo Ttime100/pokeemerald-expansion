66315061b865969534bd7bf8602de6ab0d211f78
Tyler <tylerjklein24@gmail.com>
2026-05-08 15:42:08 -0400

    Updated the Lottery Corner Prizes

--- src/lottery_corner.c
+++ src/lottery_corner.c
@@ -13,9 +13,9 @@ static EWRAM_DATA u16 sOtIdDigit = 0;
 
 static const u16 sLotteryPrizes[] =
 {
-    ITEM_PP_UP,
-    ITEM_EXP_SHARE,
-    ITEM_MAX_REVIVE,
+    ITEM_PP_MAX,
+    ITEM_BOTTLE_CAP,
+    ITEM_GOLD_BOTTLE_CAP,
     ITEM_MASTER_BALL,
 };
 
